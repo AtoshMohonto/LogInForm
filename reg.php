d6e32beaa1941a148719e6101aee5fef2f542210
Atosh Mohonto <135542605+AtoshMohonto@users.noreply.github.com>
2024-11-22 21:44:43 +0600

    post

--- reg.php
+++ reg.php
@@ -1,9 +1,9 @@
 Registration Page
-<form action="regconfirm.php">
+<form action="regconfirm.php" method="POST">
 
 <input type="text" name="un" placeholder="Enter Your Full Name" required>
 <br>
-<input type="password" name="p">
+<input type="password" name="p" required>
 <br>
 <input type="submit" value="Registration">
 
--- regconfirm.php
+++ regconfirm.php
@@ -1,8 +1,15 @@
 Registration Confirm Page
 
 <?php
-$username=$_GET['un'];
-$password=$_GET['p'];
+if (isset($_POST['un']) && isset($_POST['p']) ){
+    $username=$_POST['un'];
+    $password=$_POST['p'];
+}
+
+else{
+    header("Location : reg.php");
+}
 ?>
-<h1>Username=<?php echo $username ?> </h1>
-<h1>Password=<?php echo $username ?> </h1>
+
+<h1>Username= <?php echo $username ?> </h1>
+<h1>Password= <?php echo $password ?> </h1>
